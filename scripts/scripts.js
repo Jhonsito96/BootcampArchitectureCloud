f3e3db017d5903c26b7eb780dbc90136469cb407
Jhonsito96 <142361195+Jhonsito96@users.noreply.github.com>
2024-08-29 20:47:52 -0500

    main

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+// Puedes agregar interactividad o funcionalidades adicionales aquí
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Hoja de vida cargada correctamente');
+});
